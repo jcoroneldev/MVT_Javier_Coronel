1bbf8d47a52668cd0a37bfb5968c1088a170fd3d
Javier Coronel <javiercoronel1@hotmail.com>
2022-04-05 13:52:51 -0300

    entrega lista

--- familycoder/familycoderapp/views.py
+++ familycoder/familycoderapp/views.py
@@ -8,13 +8,16 @@ from familycoderapp.models import *
 # Create your views here.
 
 def familia(request):
-     try:
+    if request.GET: 
+        try:
+               
           data = request.GET['nombre']
           print (type(data))
-          if data:
+          if data :
                familiar1 = familiar.objects.get( nombre = data )
-               print (type(familiar1))
-               return render(request,"familycoderapp/base.html", {"familiar": familiar1[0]})
-     except:
+               print (familiar1.nombre)
+               return render(request,"familycoderapp/base.html", {"familiar": familiar1})
+        except:
           return render(request,"familycoderapp/base.html")
+    return render(request,"familycoderapp/base.html")
     
